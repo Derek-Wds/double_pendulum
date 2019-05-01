e2f3623bac8efadbdc875ff0db8e27db94061621
Jactus <dw1920@nyu.edu>
2019-05-01 16:13:07 -0400

    add angle update

--- sketch.js
+++ sketch.js
@@ -5,28 +5,28 @@ let r1 = 200;
 let r2 = 200;
 let m1 = 10;
 let m2 = 10;
-let a1 = 0;
-let a2 = 0;
-let canvas;
-let px2 = -1;
-let py2 = -1;
+let a1 = Math.PI/3;
+let a2 = Math.PI/4;
 let g = 9.8;     // constant g in physics
 let a1_v = 0;    // velocity of angle 1
 let a2_v = 0;    // velocity of angle 2
 let a1_acc = 0;  // acceleration of angle 1
 let a2_acc = 0;  // acceleration of angle 2
-
+let canvas;
+let px2 = -1;
+let py2 = -1;
+let points = [];
 
 function setup() {
   createCanvas(1200, 1200);
   canvas = createGraphics(100, 100);
   canvas.background(255);
-
 }
 
 function draw() {
   // set up
-  image(canvas, 0, 0)
+  background(255);
+  image(canvas, 0, 0);
   stroke(0);
   strokeWeight(2);
   translate(600, 300);
@@ -49,21 +49,44 @@ function draw() {
   ellipse(x2, y2, m2, m2);
 
   // update the angle a1 and a2
-  a1 += a1_v;
-  a2 += a2_v
-
-  
-
+  a1 += 0.1;
+  a2 -= 0.2;
+  // a1 += a1_v;
+  // a2 += a2_v
+  // update_a1();
+  // update_a2();
+  // a1_v += a1_acc;
+  // a2_v += a2_acc;
+  points.push([x2, y2]);
 
   // draw
   canvas.translate(600, 200);
   canvas.strokeWeight(4);
   canvas.stroke(0);
-  if (frameCount > 1) {
-    canvas.line(x2, y2, px2, py2);
+  for (i = 1; i < points.length; i++){
+    canvas.line(points[i][0], points[i][1], points[i-1][0], points[i-1][1]);
   }
   
   px2 = x2;
   py2 = y2;
 }
 
+// function to update angle a1
+function update_a1() {
+  let term1 = -g * (2 * m1 + m2) * sin(a1);
+  let term2 = -m2 * g * sin(a1 - 2 * a2);
+  let term3 = Math.pow(a2_v, 2) * r2 + Math.pow(a1_v, 2) * r1 * cos(a1-a2);
+  let term4 = -2 * sin(a1 - a2) * m2 * (term3);
+  let term5 = r1 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a1));
+  a1_acc = (term1 + term2 + term4) / term5;
+}
+
+// function to update angle a2
+function update_a2() {
+  let term1 = 2 * sin(a1 - a2);
+  let term2 = (Math.pow(a1_v, 2) * r1 * (m1 + m2));
+  let term3 = g * (m1 + m2) * cos(a1);
+  let term4 = Math.pow(a2_v, 2) * r2 * m2 * cos(a1 - a2);
+  let term5 = r2 * (2*m1 + m2 - m2 * cos(2 * a1 - 2 * a2));
+  a2_acc = (term1 * (term2 + term3 + term4)) / term5;
+}
