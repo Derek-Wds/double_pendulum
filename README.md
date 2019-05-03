20aa64d63bfff2d6a03a443b65c7f5c354e9eab0
Jactus <dw1920@nyu.edu>
2019-05-03 15:46:59 -0400

    DONE

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# double_pendulum
+## This is a simulation project for double pendulum
+
+* The original idea was come from the interesting little GIF on this [web page](https://en.wikipedia.org/wiki/Chaos_theory).
+* Thanks for scientists and researchers from MIT and USYD to provide the numerical equations. Here are the links to them: [MIT](http://web.mit.edu/jorloff/www/chaosTalk/double-pendulum/double-pendulum-en.html), [USYD](http://www.physics.usyd.edu.au/~wheat/dpend_html/).
--- sketch.js
+++ sketch.js
@@ -6,12 +6,10 @@ let r2 = 200;
 let m1 = 15;
 let m2 = 10;
 let a1 = Math.PI/3;
-let a2 = Math.PI/2;
+let a2 = Math.PI/6;
 let g = 9.8;     // constant g in physics
 let a1_v = 0;    // velocity of angle 1
 let a2_v = 0;    // velocity of angle 2
-let a1_acc = 0;  // acceleration of angle 1
-let a2_acc = 0;  // acceleration of angle 2
 let canvas;
 let px2 = -1;
 let py2 = -1;
@@ -50,12 +48,12 @@ function draw() {
   ellipse(x2, y2, m2, m2);
 
   // update the angle a1 and a2
-  a1 += a1_v;
-  a2 += a2_v
-  update_a1();
-  update_a2();
+  let a1_acc = update_a1();
+  let a2_acc = update_a2();
   a1_v += a1_acc;
   a2_v += a2_acc;
+  a1 += a1_v;
+  a2 += a2_v
   points.push([x2, y2]);
 
   // draw
@@ -75,20 +73,47 @@ function draw() {
 
 // function to update angle a1
 function update_a1() {
-  let term1 = -g * (2 * m1 + m2) * sin(a1);
-  let term2 = -m2 * g * sin(a1 - 2 * a2);
-  let term3 = Math.pow(a2_v, 2) * r2 + Math.pow(a1_v, 2) * r1 * cos(a1 - a2);
-  let term4 = -2 * sin(a1 - a2) * m2 * (term3);
-  let term5 = r1 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2));
-  a1_acc = (term1 + term2 + term4) / term5;
+  let term1 = m2 * r1 * Math.pow(a1_v, 2) * sin(a2 - a1) * cos(a2 - a1);
+  let term2 = m2 * g * sin(a2) * cos(a2 - a1);
+  let term3 = m2 * r2 * Math.pow(a2_v, 2) * sin(a2 - a1);
+  let term4 = -(m1 + m2) * g * sin(a1);
+  let term5 = (m1 + m2) * r1 - m2 * r1 * Math.pow(cos(a2 - a1), 2);
+  let a1_acc = (term1 + term2 + term3 + term4) / term5;
+  return a1_acc;
 }
 
 // function to update angle a2
 function update_a2() {
-  let term1 = 2 * sin(a1 - a2);
-  let term2 = Math.pow(a1_v, 2) * r1 * (m1 + m2);
-  let term3 = g * (m1 + m2) * cos(a1);
-  let term4 = Math.pow(a2_v, 2) * r2 * m2 * cos(a1 - a2);
-  let term5 = r2 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2));
-  a2_acc = (term1 * (term2 + term3 + term4)) / term5;
+  let term1 = -m2 * r2 * Math.pow(a2_v, 2) * sin(a2 - a1) * cos(a2 - a1);
+  let term2 = m1 + m2;
+  let term3 = g * sin(a1) * cos(a2 - a1);
+  let term4 = -r1 * Math.pow(a1_v, 2) * sin(a2 - a1);
+  let term5 = -g * sin(a2);
+  let term6 = (m1 + m2) * r2 - m2 * r2 * Math.pow(cos(a2 - a1), 2);
+  let a2_acc = (term1 + term2 * (term3 + term4 + term5)) / term6;
+  return a2_acc;
 }
+
+
+// // This is the equation provided by MIT
+// function update_a1() {
+//   let term1 = -g * (2 * m1 + m2) * sin(a1);
+//   let term2 = -m2 * g * sin(a1 - 2 * a2);
+//   let term3 = Math.pow(a2_v, 2) * r2 + Math.pow(a1_v, 2) * r1 * cos(a1 - a2);
+//   let term4 = -2 * sin(a1 - a2) * m2 * (term3);
+//   let term5 = r1 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2));
+//   let a1_acc = (term1 + term2 + term4) / term5;
+
+//   return a1_acc;
+// }
+
+// function update_a2() {
+//   let term1 = 2 * sin(a1 - a2);
+//   let term2 = Math.pow(a1_v, 2) * r1 * (m1 + m2);
+//   let term3 = g * (m1 + m2) * cos(a1);
+//   let term4 = Math.pow(a2_v, 2) * r2 * m2 * cos(a1 - a2);
+//   let term5 = r2 * (2 * m1 + m2 - m2 * cos(2 * a1 - 2 * a2));
+//   let a2_acc = (term1 * (term2 + term3 + term4)) / term5;
+
+//   return a2_acc;
+// }
